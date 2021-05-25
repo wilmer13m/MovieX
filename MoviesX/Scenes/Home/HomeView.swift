f19b8c7fd9e30c9fc85e27d6dc80f2584a859aaf
wilmer mendoza <wilmer.mendoza@elitgon.com>
2021-05-25 15:27:25 -0400

    Development Wilmer Mendoza: Se creo la maquetacion de la grilla del home

--- MoviesX/Scenes/Home/HomeView.swift
+++ MoviesX/Scenes/Home/HomeView.swift
@@ -11,13 +11,35 @@ struct HomeView: View {
     
     @State private var selection = 0
     
+    private var symbols = ["keyboard", "hifispeaker.fill", "printer.fill", "tv.fill", "desktopcomputer", "headphones", "tv.music.note", "mic", "plus.bubble", "video"]
+    private var gridItemLayout = Array(repeating: GridItem(.flexible(), spacing: 0), count: 2)
+    private var colors: [Color] = [.yellow, .purple, .green]
+
+    
     var body: some View {
+        
         NavigationView(content: {
+            
             VStack {
+                
                 TabView(selection: $selection) {
-                   MovieMinitureView()
-                    .frame(width: 200, height: 300)
-                        .tag(0)
+                    
+                    ScrollView {
+                        LazyVGrid(columns: gridItemLayout, spacing: 8) {
+                            // Display the item
+                            ForEach(symbols, id: \.self) { travel in
+                                MovieMinitureView()
+                                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 300)
+                                    .padding(.horizontal, 4)
+                            }
+                        }
+                        .padding(.top, 10)
+                        .padding(.horizontal, 10)
+                        .background(Color("black1"))
+                    }
+                    .padding(.bottom, 139)
+                    .tag(0)
+                    
                     Text("Top Rated")
                         .tag(1)
                     Text("Upcoming")
@@ -25,12 +47,15 @@ struct HomeView: View {
                     Text("Now Playing")
                         .tag(3)
                 }
+                
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                
+
                 Divider()
-                
+
                 TabBarView(selection: $selection)
+                    .ignoresSafeArea()
             }
+            .ignoresSafeArea()
             .navigationBarTitle("Movies", displayMode: .automatic)
             .blueNavigation
         })
